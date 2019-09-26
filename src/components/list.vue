2ac99e71501560eaf3baeead5874c70298ea182c
matao6 <1152843670@qq.com>
2019-09-26 17:08:24 +0800

    list.vue inject json data

--- src/components/list.vue
+++ src/components/list.vue
@@ -13,17 +13,17 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th>accountId</th>
-          <td>getCompanyName</td>
-          <td>getCompanyEmail</td>
-          <td>companyType</td>
-          <td>companyStatus</td>
-          <td></td>
+        <tr v-for="item in items" v-bind:key="item.id">
+          <th>{{item.id}}</th>
+          <td>{{item.name}}</td>
+          <td>{{item.email}}</td>
+          <td>{{item.companyType}}</td>
+          <td>{{item.companyStatus}}</td>
+          <td>{{item.registerTime}}</td>
           <td>
-            <button class="btn btn-sm btn-primary mr-2">查看</button>
-            <button class="btn btn-sm btn-warning mr-2">编辑</button>
-            <button class="btn btn-sm btn-danger mr-2">删除</button>
+            <button class="btn btn-sm btn-primary mr-2" v-bind:murl="'?id='+item.id" v-bind:mid="item.id">查看</button>
+            <button class="btn btn-sm btn-warning mr-2" v-bind:murl="'?id='+item.id" v-bind:mid="item.id">编辑</button>
+            <button class="btn btn-sm btn-danger mr-2" v-bind:murl="'?id='+item.id" v-bind:mid="item.id">删除</button>
           </td>
         </tr>
       </tbody>
@@ -39,7 +39,7 @@
                   <a class="page-link" href="?page=1">1</a>
                 </li>
                 <li class="page-item">
-                  <a class="page-link" href="">尾页</a>
+                  <a class="page-link" href>尾页</a>
                 </li>
               </ul>
             </nav>
@@ -52,13 +52,49 @@
 
 <script>
 export default {
-  name: 'list',
-  data: function(){
-    
+  name: "list",
+  data: function() {
+    return listData;
   }
-}
+};
+
+var listData = {
+  items: [
+    {
+      id: 1,
+      name: "n",
+      email: '11@163.com',
+      companyType: '有限',
+      companyStatus: '1',
+      registerTime: '219-08-06'
+    },
+    {
+      id: 2,
+      name: "a",
+      email: '22@163.com',
+      companyType: '有限',
+      companyStatus: '2',
+      registerTime: '219-08-06'
+    },
+    {
+      id: 3,
+      name: "m",
+      email: '33@163.com',
+      companyType: '有限',
+      companyStatus: '1',
+      registerTime: '219-08-06'
+    },
+    {
+      id: 4,
+      name: "e",
+      email: '44@163.com',
+      companyType: '有限',
+      companyStatus: '2',
+      registerTime: '219-08-06'
+    }
+  ]
+};
 </script>
 
 <style>
-
 </style>
