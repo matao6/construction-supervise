3a36cb14b3381979a8a5440068d3120d410bdbfc
matao6 <1152843670@qq.com>
2019-11-01 13:44:21 +0800

    test get hidden id

--- src/components/message.vue
+++ src/components/message.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="m_msg">
-      test message template
+      test message撒扩大解放螺丝钉解放上空的飞机流口水的十六的客服极乐世界的分类收集东方科技 template---{{id}}
   </div>
 </template>
 
 <script>
 export default {
+  data: function(){
+    return {
+      id: null
+    }
+  },
+  created:function(){
+    this.id = $('#h_id').val()
+  }
     // mounted: function(){
     //     $('.m_msg').parent().parent().parent().css('width','80%');
     // },
