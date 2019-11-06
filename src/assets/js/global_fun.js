9c457c461cd1d3de82bb44b51992b0380b6d458b
matao6 <1152843670@qq.com>
2019-11-06 17:30:27 +0800

    global function and variable

---
+++ src/assets/js/global_fun.js
@@ -0,0 +1,11 @@
+// param为传入参数
+function g_getData(param) {
+    alert(param)
+}
+
+export default {
+    // Vue.js的插件应当有一个公开方法 install。这个方法的第一个参数是 Vue 构造器，第二个参数是一个可选的选项对象。
+    install: function (Vue) {
+        Vue.prototype.g_getData = (param) => g_getData(param)
+    }
+}
--- src/assets/js/global_variable.js
+++ src/assets/js/global_variable.js
@@ -1,4 +1,4 @@
-const m_mainUrl='http://192.168.0.192:8080';
+const m_mainUrl='http://192.168.0.164:8081';
 
 export default{
     m_mainUrl
