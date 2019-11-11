dc6917a6fdef2aa2ec96bb50d59b9f7eeaff8c5d
matao6 <1152843670@qq.com>
2019-11-11 09:14:15 +0800

    edit info function and add new info function

--- src/components/xinwen_list.vue
+++ src/components/xinwen_list.vue
@@ -11,11 +11,12 @@
         <i slot="prefix" class="el-input__icon el-icon-search"></i>
       </el-input>
       <el-button type="primary" size="medium" style="margin-left: 15px;" @click="searchInfo">搜索</el-button>
+      <el-button type="info" size="medium" style="margin-left: 15px;" @click="showAdd">添加新闻</el-button>
     </div>
     <el-table :data="items" border style="width: 100%">
       <el-table-column prop="id" label="ID" width="80"></el-table-column>
-      <el-table-column prop="author" label="作者" width="120"></el-table-column>
       <el-table-column prop="title" label="标题" width="180"></el-table-column>
+      <el-table-column prop="author" label="作者" width="120"></el-table-column>
       <el-table-column label="内容" width="330">
         <template slot-scope="scope">
           <div>{{scope.row.content}}</div>
@@ -51,6 +52,8 @@
 import Pagination from "./pagination.vue";
 import Check from "./xinwen_check.vue";
 import Editor from "./xinwen_edit.vue";
+import Add from "./xinwen_add.vue";
+// import Vue from 'vue'
 
 export default {
   name: "list",
@@ -61,7 +64,7 @@ export default {
       // 分页数据
       currentPage: 0,
       total: 0,
-      size: 15,
+      size: 5,
       // 本地href
       nowRouter: location.href,
       // el-input
@@ -73,6 +76,7 @@ export default {
     };
   },
   props: {
+    // 测试tag
     tag: Boolean
   },
   components: {
@@ -84,15 +88,15 @@ export default {
     // 从子组件xinwen_edit中，监控refresh tag无刷新获取获取数据
     refresh(val) {
       var now = this.refresh;
-      console.log("watch1", now);
+      // console.log("watch1", now);
       if (now == "false") {
-        console.log("watch2", now);
+        // console.log("watch2", now);
         var getPage = document.querySelector(".el-pager .number.active")
           .innerText;
         // 刷新当前列表
         this.changePage(getPage);
         // 关闭弹框
-        $('.v-modal').click();
+        $(".v-modal").click();
         // 重置refresh tag
         this.refresh = true;
       }
@@ -103,6 +107,7 @@ export default {
     }
   },
   methods: {
+    // 显示查看
     showCheck(event) {
       document.querySelector("#h_id").value = event.target.getAttribute("mid");
       const h = this.$createElement;
@@ -121,6 +126,7 @@ export default {
           // })
         });
     },
+    // 显示编辑
     showEdit(event) {
       document.querySelector("#h_id").value = event.target.getAttribute("mid");
       const h = this.$createElement;
@@ -139,22 +145,80 @@ export default {
           // })
         });
     },
-    open() {
+    // 显示添加新闻
+    showAdd(event) {
+      document.querySelector("#h_id").value = event.target.getAttribute("mid");
+      const h = this.$createElement;
+      var that = this;
+      this.$msgbox({
+        title: "新增",
+        message: h(Add, { key: that.key++ }),
+        showCancelButton: false,
+        showConfirmButton: false
+      })
+        .then(action => {})
+        .catch(() => {
+          // this.$message({
+          //   type: 'info',
+          //   message: '已取消删除'
+          // })
+        });
+    },
+    // 删除新闻
+    open(event) {
+      var that = this;
+      var m_id = event.target.getAttribute("mid");
+      // console.log(m_id);
       this.$confirm("确定删除吗?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning"
       })
-        .then(() => {})
+        .then(() => {
+          // 加载浮层
+          const loading = this.$loading({
+            lock: true,
+            text: "Loading",
+            spinner: "el-icon-loading",
+            background: "rgba(0, 0, 0, 0.7)"
+          });
+          // 删除接口
+          this.axios({
+            url: that.GLOBAL.m_mainUrl + "/webnews/webnews/" + m_id,
+            method: "delete"
+          })
+            .then(function(response) {
+              loading.close();
+              if (response.status == 200) {
+                that.$message({
+                  message: response.data.message,
+                  duration: 2000,
+                  type: "success"
+                });
+                // 根据refresh tag刷新祖父级组件
+                document.querySelector("#h_refresh").value = false;
+                document
+                  .querySelector("#h_refresh")
+                  .dispatchEvent(new Event("input"));
+              }
+            })
+            .catch(function(error) {
+              loading.close();
+              console.log(error);
+            });
+        })
         .catch(() => {});
     },
+    // 跳转分页
     changePage(page) {
       var that = this;
       // console.log(page);
       this.axios
-        .get(that.GLOBAL.m_mainUrl + "/webnews/lists", {
+        .get(that.GLOBAL.m_mainUrl + "/webnews/searchLists", {
           params: {
-            page: page
+            page: page,
+            title: that.input,
+            size: that.size
           }
         })
         .then(function(response) {
@@ -172,12 +236,14 @@ export default {
           console.log(error);
         });
     },
+    // 搜索标题
     searchInfo() {
       var that = this;
       this.axios
-        .get(that.GLOBAL.m_mainUrl + "/webnews/lists", {
+        .get(that.GLOBAL.m_mainUrl + "/webnews/searchLists", {
           params: {
-            companyName: that.input
+            title: that.input,
+            size: that.size
           }
         })
         .then(function(response) {
@@ -198,12 +264,21 @@ export default {
     }
   },
   created: function() {
+    // console.log(Vue.version)
+    // 加载浮层
+    const loading = this.$loading({
+      lock: true,
+      text: "Loading",
+      spinner: "el-icon-loading",
+      background: "rgba(0, 0, 0, 0.7)"
+    });
     var that = this;
     this.axios({
       method: "get",
       url: that.GLOBAL.m_mainUrl + "/webnews/lists"
     })
       .then(function(response) {
+        loading.close();
         // console.log(response);
         if (response.status == 200) {
           let mData = response.data.data;
@@ -215,6 +290,7 @@ export default {
       })
       .catch(function(error) {
         // 请求失败处理
+        loading.close();
         console.log(error);
       });
   }
